5e20701c6e9389b2644ba3a53ff8ecdc2319f3a7
smruti-rekhad <smrutirekhad484@gmail.com>
2025-02-04 23:31:42 +0530

    Add files via upload

---
+++ dsa.java
@@ -0,0 +1,15 @@
+public class dsa {
+  public static void main(String[]args){
+    int []marks=new int[5];
+    marks[0]=12;
+    marks[1]=33;
+    marks[2]=33;
+    marks[3]=22;
+    marks[4]=32;
+    marks[5]=42;
+    
+  for(int i=0;i<=5;i++){
+  System.out.println(marks[i]);
+  }
+}
+}
