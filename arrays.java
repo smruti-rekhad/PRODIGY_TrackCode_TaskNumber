255fbc7eb51090aa3e087556f22fafb60b9fb3cd
smruti-rekhad <smrutirekhad484@gmail.com>
2025-02-04 23:30:57 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+    public static void main(String[]args){
+    
+    int myIntArr[] = { 1, 5, 993, 35 };
+
+for (int b = 0; b < myIntArr.length; b++) {
+System.out.println(myIntArr[b]);
+}
+    }
+}
